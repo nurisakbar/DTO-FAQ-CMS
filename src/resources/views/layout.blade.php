bf9e64ff9f6b6b99b52509b83a66de2cd46e2f5a
Daniel Handoko <danielhandoko@outlook.com>
2022-02-18 08:59:45 +0700

    + add favicon

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -8,8 +8,10 @@
 
     <title> @yield('title')</title>
 
-   
 
+
+    <link rel="icon" href="{{ url('https://faq.kemkes.go.id/storage/icon1635923574.png') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
