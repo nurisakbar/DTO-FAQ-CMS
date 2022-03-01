f4f302b533b121e2df08aa009cb6538614d7ede4
Daniel Handoko <danielhandoko@outlook.com>
2022-03-01 16:40:08 +0700

    + fix missing favicon

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -8,9 +8,7 @@
 
     <title> @yield('title')</title>
 
-
-
-    <link rel="icon" href="{{ url('https://faq.kemkes.go.id/storage/icon1635923574.png') }}">
+    <link rel="shortcut icon" href="{{ url('https://faq.kemkes.go.id/storage/icon1635923574.png') }}" />
     <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
